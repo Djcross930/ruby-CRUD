ebb16e626c9a4dd7a297e670c4ab6bac13eb94cb
Dan Cross <dancross96@gmail.com>
2022-07-28 19:41:05 -0500

    added the post create route and create method

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,10 +1,22 @@
 class DogsController < ApplicationController
+  
   def index
     dog = Dog.all
     render json: Dog.all
   end
+
   def show
     dog = Dog.find_by(id: params[:id])
+    render json: dog.as_json
+  end
+
+  def create
+    dog =Dog.new(
+      name: params[:name],
+      breed: params[:breed],
+      age: params[:age]
+    )
+    dog.save
     render json: dog.as_json
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
 get "/dogs" => "dogs#index"
 get "/dogs/:id" => "dogs#show"
-
+post "/dogs" => "dogs#create"
 
 
 end
